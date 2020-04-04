fdaf5e9b654ab99bff0e1b3c8f3d58ab1514f93a
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-04-04 14:06:22 -0700

    completed data processing/formatting

--- src/js/index.js
+++ src/js/index.js
@@ -1,63 +1,95 @@
 import { select, selectAll } from 'd3';
 import { min, max, range, csv, line } from 'd3';
-import { scaleLinear, arc, svg} from 'd3';
+import { 
+    scaleLinear, 
+    arc, 
+    svg,
+    scaleBand,
+    axisBottom,
+    axisLeft} from 'd3';
 
-console.log('testing123')
+const canvasDemensions = {
+    w: 1000,
+    h: 600,
+    grpX: 30,
+    grpY: 30
+}
+const margins = {top: 20, right:20, bottom:20, left:20}
+const innerWidth = canvasDemensions.w - (margins.right - margins.left)
+const innerHeight = canvasDemensions.h - (margins.top - margins.bottom);
+let yScale = scaleLinear().rangeRound([innerHeight, 0]);
+let xScale = scaleLinear().rangeRound([innerWidth, 0]);
+const chartData = {};
 
-//svg canvas demensions
-const cd = {
-            height: 500,
-            width: 500,
-            padding: {left:100, top: 50, right:50, bottom:50},
-            yAxis: 600
-};
-const years = '1950 1955 1960 1965 1970 1975 1980 1985 1990 1995 2000 2005 2010 2015'.split(' ')
-
-const dataURL = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
-const fectchData = csv(dataURL)
-                        .then(rawGdpData => {                            
-                            const rmvChar = /(\"|\]|\[)/g;
-                            const rawData = rawGdpData.map(d =>  Object.values(d)
-                                                                    .join('')
-                                                                    .trim()
-                                                                    .replace(rmvChar,''))
-                            return rawData
-                        })
-                        
-                        // convert Yearly GDP to Strng
-                        .then(data => {
-                            const re = /^\d*\W\d$/;
-                            const gdpData = data.filter(d => d.match(re));
-                            const convertedFunds = gdpData
-                                    .map(annualGdp => 
-                                        { return Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
-                                            .format(annualGdp)
-                                            .replace(/0$/, ' Billion')
-                                    })
-                            return [{data: data, convertedFunds:convertedFunds}]
-                        })
-                        .then(gdpDate => {
-                            const re = /^\d{4}\W\d{2}\W\d{2}$/;
-                            const years = gdpDate.map(year => year.data.match(re))
-                            console.log(years)
-                        })
-                                          
-                            
+const rmvChar = /(\"|\]|\[)/g; 
+const processData = d => Object.values(d).join('').trim().replace(rmvChar,'')
+const gdpDataURL = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+  
 
 
-const canvas = select('#chart-container')
-                .append('svg')
-                    .attr('y', cd.yAxis)
-                    .attr('x', cd.xAxis)
-                    .attr('height', cd.height)
-                    .attr('width', cd.width)
-                    .attr('fill', 'blue')
+csv(gdpDataURL)
+    .then(rawGdpData => rawGdpData.map(processData))            
+    .then(data => {
+        
+    const date = /^\d{4}\W\d{2}\W\d{2}$/;
+    const gdp = /^\d+(\W\d+)?$/;
+    const rmvUndef = d => d !== undefined;
+    const addAbillion = match => ' Billion';
+    const gdpAmount = d => d;
 
-const innerCanvas = select('svg')
-                            .append('g')
-                            .attr('transform', `translate(${cd.padding.left}, ${cd.padding.top})`)
-                            .append('rect')
-                                .attr('y',100)
-                                .attr('x', 200)
-                                .attr('width',200)
-                                .attr('height',200);
+
+    const gdpData = data.map(e => {
+        //GET YEAR
+            if (e.match(date)) {
+                const gdpYear = new Date(e);
+                return gdpYear.getUTCFullYear()
+        
+        //CONVERT TO GDP BILLIONS FOR  HOVER OUTPUT
+            } else if (e.match(gdp)) {
+                return Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
+                            .format(e.match(gdp)[0])
+                            .replace(/0$/, addAbillion)
+            }
+        }).filter(rmvUndef)
+
+    //CONVERT TO FULL YEAR X-AXIS 
+    const xAxisYear = gdpData.filter(d => typeof(d) === 'number').map(y => ['year',y])
+
+    //CONVERT TO GDP Y-AXIS
+    const yAxisGDP = data.filter(d => d.match(gdp)).map(d => ['gdp', Math.round(d)]);
+
+    xAxisYear.forEach((e,i) => {
+        chartData[i] = {
+            [e[0]]: e[1],
+            [yAxisGDP[0][0]]: yAxisGDP[i][1]
+        }
+        return;
+    })
+    console.log(chartData)
+
+
+
+
+
+
+
+
+        yScale.domain(yAxisGDP) // Returns an array of (Gdp)
+        xScale.domain(xAxisYear) // Returns an array of values ( years )
+
+        const svg = select('body')
+                        .append('g')
+                        .attr('transform', `translate(${canvasDemensions.grpX},${canvasDemensions.grpY})`)
+                            .append('svg')
+                            .attr('width', canvasDemensions.w)
+                            .attr('height', canvasDemensions.h)
+        
+        const bars = svg.selectAll('.bar')
+                        .data(data)
+                        .enter()
+                        .append('rect')
+                        .attr('x',xScale(d => d))
+                        .attr('y',yScale(d => d))
+                        .attr('height', innerHeight)
+                        // .attr('weight', )
+  });
