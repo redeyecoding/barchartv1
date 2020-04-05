20dca8bffa37e4a22e4c2aafb1e9ae4f42355a2b
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-04-05 06:31:00 -0700

    added LinearScale and Bandwidth for chart

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import { select, selectAll } from 'd3';
+import '../styles/style.scss'
 import { min, max, range, csv, line } from 'd3';
 import { 
     scaleLinear, 
@@ -8,88 +9,96 @@ import {
     axisBottom,
     axisLeft} from 'd3';
 
-const canvasDemensions = {
-    w: 1000,
-    h: 600,
-    grpX: 30,
-    grpY: 30
-}
-const margins = {top: 20, right:20, bottom:20, left:20}
-const innerWidth = canvasDemensions.w - (margins.right - margins.left)
-const innerHeight = canvasDemensions.h - (margins.top - margins.bottom);
-let yScale = scaleLinear().rangeRound([innerHeight, 0]);
-let xScale = scaleLinear().rangeRound([innerWidth, 0]);
-const chartData = {};
-
-const rmvChar = /(\"|\]|\[)/g; 
-const processData = d => Object.values(d).join('').trim().replace(rmvChar,'')
-const gdpDataURL = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
-  
-
-
-csv(gdpDataURL)
-    .then(rawGdpData => rawGdpData.map(processData))            
-    .then(data => {
-        
-    const date = /^\d{4}\W\d{2}\W\d{2}$/;
-    const gdp = /^\d+(\W\d+)?$/;
+    const canvasDemensions = {
+        w: 800,
+        h: 400,
+        grpX: 50,
+        grpY: 50
+    }
+    const gdp = d => d.gdp;
+    const margins = {top: 20, right:20, bottom:20, left:20}
+    const innerWidth = canvasDemensions.w - (margins.right - margins.left)
+    const innerHeight = canvasDemensions.h - (margins.top - margins.bottom);
+    const chartData = {};
+    const rmvChar = /(\"|\]|\[)/g;
+    const dateRegex = /^\d{4}\W\d{2}\W\d{2}$/;
+    const gdpRegex = /^\d+(\W\d+)?$/;
     const rmvUndef = d => d !== undefined;
     const addAbillion = match => ' Billion';
     const gdpAmount = d => d;
-
-
-    const gdpData = data.map(e => {
-        //GET YEAR
-            if (e.match(date)) {
-                const gdpYear = new Date(e);
-                return gdpYear.getUTCFullYear()
-        
-        //CONVERT TO GDP BILLIONS FOR  HOVER OUTPUT
-            } else if (e.match(gdp)) {
-                return Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
-                            .format(e.match(gdp)[0])
-                            .replace(/0$/, addAbillion)
+    const processData = d => Object.values(d).join('').trim().replace(rmvChar,'')
+    const gdpDataURL = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+      
+    
+    csv(gdpDataURL)
+        .then(rawGdpData => rawGdpData.map(processData))            
+        .then(data => {
+          
+        const gdpData = data.map(e => {
+            //GET YEAR
+                if (e.match(dateRegex)) {
+                    const gdpYear = new Date(e);
+                    return gdpYear.getUTCFullYear()
+            
+            //CONVERT TO GDP BILLIONS FOR  HOVER OUTPUT
+                } else if (e.match(gdpRegex)) {
+                    return Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
+                                .format(e.match(gdpRegex)[0])
+                                .replace(/0$/, addAbillion)
+                }
+            }).filter(rmvUndef)
+    
+    
+    
+    
+        //CONVERT TO FULL YEAR X-AXIS 
+        const xAxisYear = gdpData.filter(d => typeof(d) === 'number').map(y => ['year',y])
+    
+        //CONVERT TO GDP Y-AXIS
+        const yAxisGDP = data.filter(d => d.match(gdpRegex)).map(d => ['gdp', Math.round(d)]);
+    
+        xAxisYear.forEach((e,i) => {
+            chartData[i] = {
+                        [e[0]]: e[1],
+                        [yAxisGDP[0][0]]: yAxisGDP[i][1]
             }
-        }).filter(rmvUndef)
-
-    //CONVERT TO FULL YEAR X-AXIS 
-    const xAxisYear = gdpData.filter(d => typeof(d) === 'number').map(y => ['year',y])
-
-    //CONVERT TO GDP Y-AXIS
-    const yAxisGDP = data.filter(d => d.match(gdp)).map(d => ['gdp', Math.round(d)]);
-
-    xAxisYear.forEach((e,i) => {
-        chartData[i] = {
-            [e[0]]: e[1],
-            [yAxisGDP[0][0]]: yAxisGDP[i][1]
-        }
-        return;
-    })
-    console.log(chartData)
-
-
-
-
-
-
-
-
-        yScale.domain(yAxisGDP) // Returns an array of (Gdp)
-        xScale.domain(xAxisYear) // Returns an array of values ( years )
-
+            return;
+        })
+        
+    
+     
+    
+        let yScale = scaleLinear()
+                        .domain([0, max(Object.values(chartData),gdp)])
+                        .range([0, innerHeight]);
+    
+        // let xScale = scaleBand()
+        //                 .domain(Object.values(chartData).map((d => d.gdp))
+        //                 .range([0,innerWidth]).padding(.1)
+    
+    
+        console.log(yScale.domain())
+    
+    
+    
         const svg = select('body')
-                        .append('g')
-                        .attr('transform', `translate(${canvasDemensions.grpX},${canvasDemensions.grpY})`)
                             .append('svg')
                             .attr('width', canvasDemensions.w)
+                            .attr('id', 'canvas')
                             .attr('height', canvasDemensions.h)
-        
-        const bars = svg.selectAll('.bar')
-                        .data(data)
+                              .append('g')
+                              .attr('transform', `translate(${canvasDemensions.grpX},${canvasDemensions.grpY})`)
+    
+        const bar = svg.selectAll('rect')
+                        .data(Object.values(chartData))
                         .enter()
                         .append('rect')
-                        .attr('x',xScale(d => d))
-                        .attr('y',yScale(d => d))
-                        .attr('height', innerHeight)
-                        // .attr('weight', )
-  });
+                        .attr('x', 100)
+                        .attr('y', 100)
+                        // .attr('x',xScale((d,i) => d[i].year))
+                        // .attr('width', ((d,i) => xScale(d[i].year)))
+                        .attr('y',yScale(d => yScale(d.gdp)))
+                        .attr('height', (d => yScale(d.gdp)))
+                        .attr('fill',  'blue')
+                        
+      });
