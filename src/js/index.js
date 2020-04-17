21301be73de16a6661da6e763a0f45c34ee0dce6
jeffrey.mcintyre <redeyecoding@gmail.com>
2020-04-17 11:38:51 -0700

    feat: Added toolTip, data: added new ProcessedData Function

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
-import { select, selectAll } from 'd3';
 import '../styles/style.scss'
-import { max, csv } from 'd3';
 import { 
-    scaleLinear, 
+    max,
+    scaleLinear,
+    mouse,
+    select, 
     scaleTime,
     axisBottom,
     axisLeft} from 'd3';
@@ -16,66 +17,113 @@ const innerHeight = svgCanvas.height - chartPadding.top - chartPadding.bottom;
 
 fetch(gdpDataURL)
         .then(response => response.json())
-        .then(data => {
-            const gdpData = data.data;
-            const minDate = new Date(gdpData[0][0]);
-            const maxDate = new Date(gdpData[gdpData.length - 1][0])
+        .then(rawData => {
+                const gdpData = rawData.data;
+                const minDate = new Date(gdpData[0][0]);
+                const maxDate = new Date(gdpData[gdpData.length - 1][0]);
 
 
-            // X AND Y AXES
-            const yScale = d3.scaleLinear()
-                        .domain([0, d3.max(gdpData, d => d[1])])
-                        .range([innerHeight,0])            
-            const yAxisGenerator = d3.axisLeft(yScale)
+                // PROCESS DATA FOR TOOLTIP OUTPUT
+                const processData = (function() {
+                        let yearPlaceHolder, quarter = 1;
+                        const yearDateQuarter = gdpData.map(e => {
+                                let year = e[0].slice(0,4)
+                                const gdp = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
+                                                        .format(e[1])
+                                                        .replace(/0$/,' Billion')
+
+                                if ( year === yearPlaceHolder ) {
+                                        quarter += 1;
+                                        
+                                        if (quarter > 4) {
+                                                quarter = 1;
+                                        };
+                                        return [ e[0], e[1], year + ` Q${quarter}`, gdp ]
+
+                                } else {
+                                        yearPlaceHolder = year;
+                                        quarter = 1
+                                        return [ e[0], e[1], year + ` Q${quarter}`, gdp ] 
+                                }                        
+                                })                             
+                                return { processedData: yearDateQuarter };                
+                })();
+                const data = processData.processedData
 
 
+                // X AND Y AXES
+                const yScale = scaleLinear()
+                                .domain([0, max(data, (d,i,a) => a[i][1])])
+                                .range([innerHeight,0])            
+                const yAxisGenerator = axisLeft(yScale)
 
-            const xScale = d3.scaleTime()
-                        .domain([minDate, maxDate])
-                        .range([0, innerWidth])            
-            const xAxisGenerator = d3.axisBottom(xScale)
 
-                    
-            const svg = d3.select('#chart-container')
-                    .append('svg')
-                    .attr('width', svgCanvas.width)
-                    .attr('height', svgCanvas.height) 
-            
-            const chartTitle = svg.append('text')
-                                    .text('United States GDP: 1940\'s To 2015')
-                                    .attr('transform', 'translate(200,85)')
-                                    .attr('id', 'title')
-            
-            const chartGroup = svg.append('g')
-                                    .attr('transform', `translate(${chartPadding.left}, ${chartPadding.top})`)
-                                    .attr('id', 'chart-group')
-            // yAxis
-            chartGroup.append('g')
-                        .call(yAxisGenerator)
-                        .attr('transform', `translate(0,0)`)
-                        .attr('id', 'y-axis')
-                        .append('text')
-                        .attr('class', 'side-title-gdp')
-                        .attr('transform', 'translate(25,100) rotate(-90)')
-                        .text('Gross Domestic Product') 
-            
-            // xAxis
-            chartGroup.append('g')
-                    .attr('transform', `translate(0, ${innerHeight})`)
-                    .attr('id', 'x-axis')
-                    .call(xAxisGenerator)
-                                    
+                const xScale = scaleTime()
+                                .domain([minDate, maxDate])
+                                .range([0, innerWidth])            
+                const xAxisGenerator = axisBottom(xScale)
 
-            chartGroup.selectAll('rect')
-                    .data(gdpData)
-                    .enter()
-                    .append('rect')
-                    .attr('data-date', function(d){ return d[0]})
-                    .attr('data-gdp', function(d){ return d[1]})
-                    .attr('x',d => xScale(new Date(d[0])))
-                    .attr('width', svgCanvas.width / gdpData.length - chartPadding.bar)
-                    .attr('height', function(d){ return innerHeight - yScale(d[1]) })
-                    .attr('y',function(d){ return yScale(d[1]) })
-                    .attr('class', 'bar')
-})
-    
+                        
+                const svg = select('#chart-container')
+                        .append('svg')
+                        .attr('width', svgCanvas.width)
+                        .attr('height', svgCanvas.height) 
+                
+                const chartTitle = svg.append('text')
+                                        .text('United States GDP\n (1940\'s To 2015)')
+                                        .attr('transform', 'translate(120,40)')
+                                        .attr('id', 'title')
+                
+                const chartGroup = svg.append('g')
+                                        .attr('transform', `translate(${chartPadding.left}, ${chartPadding.top})`)
+                                        .attr('id', 'chart-group')
+                // yAxis
+                chartGroup.append('g')
+                                .call(yAxisGenerator)
+                                .attr('transform', `translate(0,0)`)
+                                .attr('id', 'y-axis')
+                                .append('text')
+                                .attr('class', 'side-title-gdp')
+                                .attr('transform', 'translate(25,100) rotate(-90)')
+                                .text('Gross Domestic Product') 
+                
+                // xAxis
+                chartGroup.append('g')
+                        .attr('transform', `translate(0, ${innerHeight})`)
+                        .attr('id', 'x-axis')
+                        .call(xAxisGenerator)
+
+                const toolTip = svg.append('g')
+                                        .attr('class', 'tooltip')
+                                        .style('display', 'none')
+
+                toolTip.append('text')
+                        .attr('x', 15)
+                        .attr('dy', '1.2em')
+                        .style('font-size', '1.25em')
+                                        
+
+                chartGroup.selectAll('rect')
+                        .data(data)
+                        .enter()
+                        .append('rect')
+                        .attr('data-date', function(d){ return d[0]})
+                        .attr('data-gdp', function(d){ return d[1]})
+                        .attr('x',function(d){ return xScale(new Date(d[0]) )})
+                        .attr('width', svgCanvas.width / gdpData.length - chartPadding.bar)
+                        .attr('height', function(d){ return  innerHeight - yScale(d[1]) })
+                        .attr('y',function(d){ return yScale(d[1]) })
+                        .attr('class', 'bar')
+                        .on('mouseover', function(d,i,a){
+                                toolTip.style('display', null);
+                        })
+                        .on('mouseout', function(d,i,a){
+                                toolTip.style('display','block');
+                        })
+                        .on('mousemove', function(d,i,a){
+                                const xPosition = mouse(this)[0] - 15;
+                                const yPosition = mouse(this)[1] - 55;
+                                toolTip.attr('transform', `translate(${xPosition}, ${yPosition})`)
+                                toolTip.select('text').text(`${d[2]}  ${d[3]}`)
+                        })
+        })
